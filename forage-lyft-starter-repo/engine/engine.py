625f6197e406a58fb60ac124158b10d9bcb3d139
bmaruy <97135472+bmaruy@users.noreply.github.com>
2023-09-25 00:00:20 -0700

    Delete forage-lyft-starter-repo directory

--- forage-lyft-starter-repo/engine/engine.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from car import Car
-
-class Engine(ABC):
-    @abstractmethod
-    def needs_service(self):
-        pass
--- forage-lyft-starter-repo/engine/spindler_battery.py
+++
@@ -1,9 +0,0 @@
-from battery import Battery
-
-class SpindlerBattery(Battery):
-    def __init__(self, current_date, last_service_date):
-        self.current_date = current_date
-        self.last_service_date = last_service_date
-
-    def needs_service(self):
-        return self.current_date - self.last_service_date >= 2
